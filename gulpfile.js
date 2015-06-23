f93ecfde24cb65f972adf485e758b253c20b4fb7
Scott Mathson <smathson@gmail.com>
2015-06-23 12:52:33 -0700

    add build and server tasks with watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    del = require('del'),
+    connect = require('gulp-connect'),
+    appFiles;
 
-gulp.task('connect', function() {
+appFiles = ['index.html',
+            'bower_components/**/*',
+            'src/**/*',
+            'styles/**/*',
+            'views/**/*'];
+
+gulp.task('clean', function(cb) {
+  del('build', cb);
+});
+
+gulp.task('build', ['clean'], function() {
+  return gulp.src(appFiles, {base: '.'})
+             .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(appFiles, ['build']);
+});
+
+gulp.task('server', function() {
   connect.server({
-    root: '.',
-    livereload: true
+    root: 'build'
   });
 });
 
-gulp.task('default', ['connect']);
+gulp.task('default', ['build', 'server', 'watch']);
